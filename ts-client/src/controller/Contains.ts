523a3412de4881492431094747af8fea3ab5c256
Paul Finkbeiner <paul-d-f@web.de>
2021-05-06 21:14:00 +0200

    removed client

--- ts-client/src/controller/Contains.ts
+++ ts-client/src/controller/Contains.ts
@@ -1,20 +1,21 @@
-import FormattedNumber from "../model/FormattedNumber";
+import { Contact } from "../model/Contact";
 
-export function Contains(list: FormattedNumber[], entry: FormattedNumber): boolean {
+export function Contains(list: Contact[], entry: Contact): boolean {
 
     let contains: boolean = false;
 
     let templist = list.filter(item => item.id !== entry.id)
 
-    if ((templist.find(s => s.countryCode === entry.countryCode 
-        && s.durchwahl === entry.durchwahl 
-        && s.number === entry.number
-        && s.ortsvorwahl === entry.ortsvorwahl
+    if (
+        (templist.find( contact => 
+            contact.salutation === entry.salutation && 
+            contact.title === entry.title && 
+            contact.gender === entry.gender && 
+            contact.firstname === entry.firstname && 
+            contact.lastname === entry.lastname
         ) !== undefined)) {
         contains = true;
     }
 
-
     return contains;
-
 }

e23974081abb2097f7c0dfc8b243a815ff73d107
Paul Finkbeiner <paul-d-f@web.de>
2021-05-07 17:32:43 +0200

    created new Project

--- splittr/src/App.tsx
+++ splittr/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
